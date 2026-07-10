f6d90ddc868656482be1b041ebaa5ba649568031
𝓒𝓱𝓲𝓲 𝓜𝓪𝓰𝓷𝓾𝓼 <151696847+chiimagnus@users.noreply.github.com>
2026-07-11 02:19:13 +0800

    Move practice settings into a window

--- LonelyPianistAVP/Models/WindowID.swift
+++ LonelyPianistAVP/Models/WindowID.swift
@@ -4,4 +4,5 @@ enum WindowID {
     static let preparation = "preparation"
     static let library = "library"
     static let practice = "practice"
+    static let practiceSettings = "practice-settings"
 }
